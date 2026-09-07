ba67dec379b03acd2986de2e57ea351296585f47
ardhailhamkusuma <ardhailhamkusuma9@gmail.com>
2026-09-07 16:16:12 +0700

    open.c

--- fs/open.c
+++ fs/open.c
@@ -363,10 +363,6 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 #endif
 
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_MANUAL_HOOK)
-	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
-#endif
-
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
 
@@ -832,6 +828,9 @@ cleanup_file:
  *
  * NB: the dentry reference is _not_ consumed.  If, for example, the dentry is
  * the return value of d_splice_alias(), then the caller needs to perform dput()
+ * on it after finish_open().
+ *
+ * On successreturn value of d_splice_alias(), then the caller needs to perform dput()
  * on it after finish_open().
  *
  * On successful return @file is a fully instantiated open file.  After this, if
